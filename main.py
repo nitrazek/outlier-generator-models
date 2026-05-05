55c2d34e05105f4b14e52de22c4979c58b56fd1a
sh4dqwx <bart.karolczak@gmail.com>
2026-05-05 21:01:09 +0200

    added generate_uniform_outliers to OutlierGenerator

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from outlier-generator-models!")
-
-
-if __name__ == "__main__":
-    main()
--- src/outlier_generator.py
+++ src/outlier_generator.py
@@ -1,13 +1,46 @@
+import numpy as np
+from sklearn import utils
+
+
 class OutlierGenerator:
     def __init__(self, estimator):
         self._estimator = estimator
+        self._outliers_fn = "uniform"
+        self._outlier_ratio = 0.2
+        self._outlier_margin = 0.1
+        self._random_state = 42
 
-    # TODO
     def _generate_outliers(self, X):
-        pass
+        outliers_fn_map = {
+            "uniform": self._generate_uniform_outliers
+        }
 
-    def fit(self, X, y):
-        return self._estimator.fit(X, y)
+        return outliers_fn_map[self._outliers_fn](X)
+
+    def _generate_uniform_outliers(self, X):
+        n_samples, n_features = X.shape
+        n_outliers = int(n_samples * self._outlier_ratio)
+        
+        mins = np.min(X, axis=0)
+        maxs = np.max(X, axis=0)
+        ranges = maxs - mins
+        
+        lower_bounds = mins - (ranges * self._outlier_margin)
+        upper_bounds = maxs + (ranges * self._outlier_margin)
+        
+        outliers = np.random.uniform(low=lower_bounds, high=upper_bounds, size=(n_outliers, n_features))
+
+        return outliers
+
+    def fit(self, X, y = None):
+        outliers = self._generate_outliers(X)
+        y = np.hstack([np.ones(len(X)), np.zeros(len(outliers))])
+        print(y.shape)
+        X = np.vstack([X, outliers])
+        print(X.shape)
+        X, y = utils.shuffle(X, y, random_state=self._random_state)
+        self._estimator.fit(X, y)
+        return self
 
     def predict(self, X):
         return self._estimator.predict(X)
